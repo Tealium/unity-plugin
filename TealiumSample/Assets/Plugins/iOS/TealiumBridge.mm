706eb013ea0ae77dfbd880dfa5624650169eba97
George Webster <accounts@ethyreal.com>
2015-04-10 12:19:48 -0700

    updated TealiumBridge to use the current API's update to use recommend cString NSString creation foundation helpes

--- TealiumSample/Assets/Plugins/iOS/TealiumBridge.mm
+++ TealiumSample/Assets/Plugins/iOS/TealiumBridge.mm
@@ -1,50 +1,46 @@
-NSString* CreateNSString (const char* string, NSString *fallback) {
-	if (string) {
-		return [NSString stringWithUTF8String: string];
-	} else {
-		return fallback;
-	}
-}
 
-NSString* CreateNSString (const char* string) {
-    return CreateNSString(string, @"");
+NSString * TEALStringFromCString (const char* string) {
+    return [NSString stringWithCString:string
+                              encoding:NSUTF8StringEncoding];
 }
 
 #import <TealiumLibrary/Tealium.h>
 #import <Foundation/NSJSONSerialization.h>
 
 extern "C" {
-	void _TealiumInitialize(const char* accountName, const char* profileName, const char* environmentName, int options) {
-        [Tealium initSharedInstance:CreateNSString(accountName)
-                            profile:CreateNSString(profileName)
-                             target:CreateNSString(environmentName)
+    void _TealiumInitialize(const char* accountName, const char* profileName, const char* environmentName, int options) {
+        [Tealium initSharedInstance:TEALStringFromCString(accountName)
+                            profile:TEALStringFromCString(profileName)
+                             target:TEALStringFromCString(environmentName)
                             options:options];
         
-	}
-	
-	static NSMutableDictionary *newDispatch = nil;
-	
-	void _TealiumTrackPrepare(int capacity) {
-		newDispatch = [NSMutableDictionary dictionaryWithCapacity: capacity];
-	}
-	
-	void _TealiumTrackSet(const char* key, const char* value) {
-		if(newDispatch) {
-			[newDispatch setObject: CreateNSString(value) 
-							forKey: CreateNSString(key)];
-		} else {
-			NSLog(@"_TealiumTrackSet(const char*, const char*) called before _TealiumTrackPrepare(int)");
-		}
-	}
-	
-	void _TealiumTrackSend(const char* eventType) {
-		if(newDispatch) {
-	        [[Tealium sharedInstance] track: nil
-	                             customData: newDispatch
-	                                     as: CreateNSString(eventType)];
-			newDispatch = nil;
-		} else {
-			NSLog(@"_TealiumTrackSend(const char*) called before _TealiumTrackPrepare(int)");
-		}
-	}
+        
+    }
+    
+    static NSMutableDictionary *newDispatch = nil;
+    
+    void _TealiumTrackPrepare(int capacity) {
+        newDispatch = [NSMutableDictionary dictionaryWithCapacity: capacity];
+    }
+    
+    void _TealiumTrackSet(const char* key, const char* value) {
+        if(newDispatch) {
+            [newDispatch setObject: TEALStringFromCString(value)
+                            forKey: TEALStringFromCString(key)];
+        } else {
+            NSLog(@"_TealiumTrackSet(const char*, const char*) called before _TealiumTrackPrepare(int)");
+        }
+    }
+    
+    void _TealiumTrackSend(const char* eventType) {
+        if(newDispatch) {
+            
+            [Tealium trackCallType:TEALStringFromCString(eventType)
+                        customData:newDispatch
+                            object:nil];
+            newDispatch = nil;
+        } else {
+            NSLog(@"_TealiumTrackSend(const char*) called before _TealiumTrackPrepare(int)");
+        }
+    }
 }
